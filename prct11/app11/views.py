1ef08b9415bd683864084ce5a9725a56ddea01c9
d ravi <108458982+ravi944@users.noreply.github.com>
2022-10-06 14:25:01 +0530

    Update views.py
    file updated to upload file dynamically

--- prct11/app11/views.py
+++ prct11/app11/views.py
@@ -11,35 +11,48 @@ def index(request):
     
     db = mysql.connect(host="localhost",user="root",password="1234",database="db_of_sample")
     cur = db.cursor()
+    
+    ## WE have to comment this when appending data to already exsting table
+    ## if we are creating table keep let it be(18,19,20 lines)
  
     cur.execute("DROP TABLE IF EXISTS EMPLOYEE")
     sql1 = "CREATE TABLE EXCEL_DATA20 ( first char(20) not null, last char(20), loc char(20) )"
     cur.execute(sql1) #table created
+    
+    
+    if request.method == 'POST':
+        loc = request.FILES['myfile']
+        if loc.name.endswith('.xlsx'):
+            p.save_book_as(file_name=str(loc),dest_file_name = 'your-new-file-out.xls')
+            a = xlrd.open_workbook("your-new-file-out.xls")
+        else:
+            a = xlrd.open_workbook(str(loc))
+        
 
-    l=list()
-    loc = ("D:\\sampl.xlsx")
-    ext = os.path.splitext(loc)
+#     l=list()
+#     loc = ("D:\\sampl.xlsx")
+#     ext = os.path.splitext(loc)
     
-    if ext[-1] == ".xlsx":
-        p.save_book_as(file_name=str(loc),dest_file_name = 'your-new-file-out.xls')
-        a = xlrd.open_workbook("your-new-file-out.xls")
-    else:
-        a = xlrd.open_workbook(str(loc))
+#     if ext[-1] == ".xlsx":
+#         p.save_book_as(file_name=str(loc),dest_file_name = 'your-new-file-out.xls')
+#         a = xlrd.open_workbook("your-new-file-out.xls")
+#     else:
+#         a = xlrd.open_workbook(str(loc))
         
-    sheet = a.sheet_by_index(0)
-    sheet.cell_value(0,0)
+        sheet = a.sheet_by_index(0)
+        sheet.cell_value(0,0)
     
-    l=list()
-    for i in range(1,sheet.nrows):
-        l.append(tuple(sheet.row_values(i)))
+        l=list()
+        for i in range(1,sheet.nrows):
+            l.append(tuple(sheet.row_values(i)))
         
-    q="insert into excel_data19(first, last,loc )values(%s,%s,%s)"
-    cur.executemany(q,l)
-    db.commit()
-    db.close()
+        q="insert into excel_data19(first, last,loc )values(%s,%s,%s)"
+        cur.executemany(q,l)
+        db.commit()
+        db.close()
     
     
-    return HttpResponse(q)
+    return render(request, 'input.html')
 
 
 
